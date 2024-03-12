0b6ff1baa1dea0be414da5fe7325d2ea58745837
sachitanmuo <twtrubiks@gmail.com>
2024-03-13 01:15:03 +0800

    basic structure

--- Desktop/NYCU/5th_semester/EDA/EDA_HW1/ROBDD.h
+++ Desktop/NYCU/5th_semester/EDA/EDA_HW1/ROBDD.h
@@ -3,7 +3,7 @@
 #include<string>
 #include<algorithm>
 #include<unordered_map>
-
+#include<<sstream>
 using namespace std;
 
 struct Node{
@@ -14,13 +14,23 @@ Node(char v, Node* l, Node* h) : var(v), low(l), high(h);
 };
 
 
-class ROBDD{
+class BDD{
 private:
     Node* root;
     unordered_map<char, Node> variables;
     vector<Node*> points;
+    string equations;
+    vector<string> ordering;
+    Node v0 = Node('0',nullptr, nullptr);
+    Node v1 = Node('1',nullptr, nullptr);
 public:
+    BDD(string& eq, vector<string> ord): equations(eq), ordering(ord){};
     Node* CreateNode(const char var, Node* low, Node* high);
+    Node* BDD_Build(const string &equation, const string &ordering, int idx, int iteration);
+    Node* old_or_new(char var, Node *a, Node *b);
+    Node* get_v0();
+    Node* get_v1();
+    void add(char, Node*);
 };
 
 
@@ -29,21 +39,5 @@ public:
 
 
 
-Node* old_or_new(string &equation, Node *a, Node *b){
 
 
-}
-
-Node* ROBDD::CreateNode(const char var, Node* low, Node* high){
-    Node* node = new Node(var, low, high);
-    return node;
-}
-
-Node* ROBDD_Build(const string &equation, const string &ordering, int idx){
-    if(idx >= ordering.size()) return nullptr;
-    Node *high, *low;
-    char var = ordering[idx++];
-    int pos = equation.find(var);
-    if(pos)
-
-}
